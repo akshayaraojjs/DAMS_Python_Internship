8d4a2efe98b868dd2063cfed31a7bf38029bf8dd
akshayaraojjs <akshayarao.parvam@gmail.com>
2026-01-22 13:15:23 +0530

    Django Full Stack Project with Employee CRUD Operation

--- Django - Project/employee_crud/employee/urls.py
+++ Django - Project/employee_crud/employee/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('employee-list', views.empList, name='emp-list'),
     path('registration-form', views.empRegistration, name='emp-form'),
+    path('<int:pk>/edit/', views.empUpdate, name='emp-update'),
+    path('<int:pk>/delete/', views.empDelete, name='emp-delete'),
 ]
--- Django - Project/employee_crud/employee/views.py
+++ Django - Project/employee_crud/employee/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee
+from .forms import EmployeeForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'employee/index.html')
 
 def empList(request):
-    return render(request, 'employee/emp_list.html')
+    employees = Employee.objects.all()
+    return render(request, 'employee/emp_list.html', {'employees': employees})
 
 def empRegistration(request):
-    return render(request, 'employee/emp_form.html')
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Employee Registered Successfully!')
+            return redirect('emp-list')
+    else:
+        form = EmployeeForm()
+    return render(request, 'employee/emp_form.html', {'form': form})
+
+def empUpdate(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Employee Details updated successfully!')
+            return redirect('emp-list')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'employee/emp_form.html', {'form': form})
+
+def empDelete(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        employee.delete()
+        messages.success(request, 'Employee Details deleted successfully!')
+        return redirect('emp-list')
+    return render(request, 'employee/emp_delete.html', {'employee': employee})
